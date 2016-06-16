f5f2da7f855639ffce6aea74c382b8d3ecedee7f
Jaco <jdippena@macalester.edu>
2016-06-16 10:42:12 -0500

    t-SNE now caches in Analyzer. Removed data.csv.

--- Constants.py
+++ Constants.py
@@ -3,6 +3,7 @@ FILE_NAME_WIKIBRAIN_VECS = "./data/vecs.tsv"
 FILE_NAME_WIKIBRAIN_NAMES = "./data/names.tsv"
 FILE_NAME_NAMES_AND_CLUSTERS = "./data/names_and_clusters.tsv"
 FILE_NAME_COORDS_AND_CLUSTERS = "./data/coords_and_clusters.tsv"
+FILE_NAME_TSNE_CACHE = "./data/cache/tsne_cache.tsv"
 
 NUM_CLUSTERS = 10  # number of clusters to generate from K-means
 TSNE_THETA = 0.5  # lower values make more accurate maps, but takes (much) longer
