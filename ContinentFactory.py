53899790939de81de1130357f2e8fee190803dd0
Brooke <bboatman@macalester.edu>
2016-06-20 15:14:55 -0500

    Added line numbering to wikibrain data

---
+++ ContinentFactory.py
@@ -0,0 +1,33 @@
+from MapRepresentations import Continent, continentListToFile
+from Constants import FILE_NAME_COUNTRIES
+import BorderFactory
+
+
+class ContinentFactory:
+    fullFeatureList = []
+
+    def __init__(self, countriesFile):
+        self.countriesFile = countriesFile
+
+    # ===== Generate JSON Data ==========
+    def _generateFeatureList(self, pointList):
+        newList = []
+        for region in pointList:
+            newContinent = Continent((0, 0))
+            for point in region:
+                newContinent.addPoint(point)
+            newList.append(newContinent)
+        return newList
+
+    def generatePolygonFile(self):
+        clusterList = BorderFactory.from_file().build().values()
+        for index, cluster in enumerate(clusterList):
+            print "Generating cluster", index
+            featureList = self._generateFeatureList(cluster)
+            self.fullFeatureList.append(featureList)
+
+        continentListToFile(self.fullFeatureList, self.countriesFile)
+
+if __name__ == "__main__":
+    continentFactory = ContinentFactory(FILE_NAME_COUNTRIES)
+    continentFactory.generatePolygonFile()
--- workflow.py
+++ workflow.py
@@ -1,6 +1,13 @@
 import luigi
 import os
+import sys
 import time
+import Constants
+import Analyzer
+
+
+analyzer = Analyzer.Analyzer(save_output=True, use_cache=False, subset=1000)
+
 
 class MTimeMixin:
     """
@@ -16,56 +23,83 @@ class MTimeMixin:
                 return obj
             else:
                 return [obj]
-    
+
         def mtime(path):
             return time.ctime(os.path.getmtime(path))
-    
+
         if not all(os.path.exists(out.path) for out in to_list(self.output())):
             return False
-            
+
         self_mtime = min(mtime(out.path) for out in to_list(self.output()))
-    
-        # the below assumes a list of requirements, each with a list of outputs. YMMV
+
+        # the below assumes a list of requirements,
+        # each with a list of outputs. YMMV
         for el in to_list(self.requires()):
             if not el.complete():
                 return False
             for output in to_list(el.output()):
                 if mtime(output.path) > self_mtime:
                     return False
-    
-        return True
-    
 
-class WikiBrain(luigi.ExternalTask):
+        return True
+
+class WikiBrainData(luigi.ExternalTask):
     def output(self):
         return (
-            luigi.LocalTarget("data/vectors.tsv"),
-            luigi.LocalTarget("data/names.tsv")
+            luigi.LocalTarget(Constants.FILE_NAME_WIKIBRAIN_NAMES),
+            luigi.LocalTarget(Constants.FILE_NAME_WIKIBRAIN_NAMES),
         )
+
+
+class WikiBrainNumbering(MTimeMixin, luigi.Task):
+    def output(self):
+        return (
+            luigi.LocalTarget(Constants.FILE_NAME_NUMBERED_VECS),
+            luigi.LocalTarget(Constants.FILE_NAME_NUMBERED_NAMES),
+        )
+
+    def requires(self):
+        return WikiBrainData()
+
+    def run(self):
+        with open(Constants.FILE_NAME_WIKIBRAIN_NAMES) as nameFile:
+            lines = nameFile.readlines()
+            with open(Constants.FILE_NAME_NUMBERED_NAMES, "a") as numberNames:
+                for i, line in enumerate(lines):
+                    numberNames.write('%d\t%s' % (i + 1, line))
+
+        with open(Constants.FILE_NAME_WIKIBRAIN_VECS) as nameFile:
+            lines = nameFile.readlines()
+            with open(Constants.FILE_NAME_NUMBERED_VECS, "a") as numberNames:
+                for i, line in enumerate(nameFile.readlines()):
+                    numberNames.write('%d\t%s' % (i + 1, line))
+
 
 class RegionClustering(MTimeMixin, luigi.Task):
     def output(self):
         return luigi.LocalTarget("data/cluster_labels.tsv")
 
     def requires(self):
-        return WikiBrain()
+        return WikiBrainNumbering()
 
     def run(self):
         f = open('data/cluster_labels.tsv', 'w')
         f.write('writing DATA\n')
         f.close()
 
+
 class Embedding(MTimeMixin, luigi.Task):
     def output(self):
         return luigi.LocalTarget("data/2d_embedding.tsv")
 
     def requires(self):
-        return WikiBrain()
+        return WikiBrainNumbering()
 
     def run(self):
         f = open('data/2d_embedding.tsv', 'w')
         f.write('writing DATA\n')
         f.close()
+
 
 class DenoiseAndAddWater(MTimeMixin, luigi.Task):
     def output(self):
@@ -79,7 +113,7 @@ class DenoiseAndAddWater(MTimeMixin, luigi.Task):
 
     def run(self):
         for fn in ("data/coords_and_clusters.tsv",
-                  "data/names_and_clusters.tsv"):
+                   "data/names_and_clusters.tsv"):
             f = open(fn, 'w')
             f.write('writing DATA\n')
             f.close()
