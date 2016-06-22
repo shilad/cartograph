e20bb9c80230c8baafa6728277c613803e84aa17
Brooke <bboatman@macalester.edu>
2016-06-22 15:15:56 -0500

    Baby steps to sync data.
    Former-commit-id: 9cf6a57997d1b7469d995e5b3ef0ea11c697792f

--- sync_data.sh
+++ sync_data.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-DATA=data
+DATA_DEST=data/labdata
+DATA_SRC=labdata@como.macalester.edu/data
 
 function die() {
     echo $@ >&2
     exit 1
 }
 
-if ! [ -f $DATA/labdata.key ]; then
+if ! [ -f data/labdata.key ]; then
     die "Please download Summer2016/Downloads/labdata.key in Google Drive and place it in the $DATA directory."
 fi
 
@@ -15,5 +16,23 @@ command=""
 targets=""
 
 if [ $# == 0 ]; then
-    echo "foo"
+	command=pull
+	target=all
+elif [ $# != 2 ]; then
+	command="$1"
+	target="$2"
+else
+	die "usage: $0 {push|pull} filename"
+fi
+
+if [ $command -eq "pull"]; then
+	if [ targets -eq "all" ]; then
+		srcs='*'
+	else
+		srcs="$DATA_SRC/$targets"
+	fi
+	rsync -avz "${srcs}" ${DATA_DEST}
+elif [ $command -eq "push" ]; then
+	if ! [ -f $DATA_DEST/labdata.key ];
+else
 fi
