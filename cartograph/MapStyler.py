7735b103653299404915fed3074ce10ba4dbf5ed
Shilad Sen <Shilad.Sen@target.com>
2016-06-23 14:37:44 -0500

    MapStyler
    Former-commit-id: 91b59f138505b59231a9026a928b3e296b077fef

--- cartograph/MapStyler.py
+++ cartograph/MapStyler.py
@@ -1,3 +1,4 @@
+import json
 import mapnik
 import Labels
 
@@ -8,14 +9,18 @@ class MapStyler:
     def makeMap(self, contourFilename, countryFilename, clusterIds, width=1200, height=600):
         self.m = mapnik.Map(width, height)
         self.m.background = mapnik.Color('white')
- 
-        self.m.append_style("contour", generateSinglePolygonStyle(contourFilename, 0.2, 1))
+        self.m.srs = '+init=epsg:3857'
+
+        js = json.load(open(contourFilename, 'r'))
+        numContours = len(js['features'])
+
+        self.m.append_style("contour", generateContourPolygonStyle(0.3, numContours))
         self.m.layers.append(generateLayer(contourFilename, "contour", "contour"))
  
-        self.m.append_style("outline", generateLineStyle("darkblue", 0.5))
+        self.m.append_style("outline", generateLineStyle("darkblue", 0.1))
         self.m.layers.append(generateLayer(contourFilename, "outline", "outline"))
  
-        self.m.append_style("countries", generateCountryPolygonStyle(countryFilename, 1.0, clusterIds))
+        self.m.append_style("countries", generateCountryPolygonStyle(countryFilename, 0.3, clusterIds))
         self.m.layers.append(generateLayer(countryFilename, "countries", "countries"))
         self.m.zoom_all()
 
@@ -23,7 +28,9 @@ class MapStyler:
         assert(self.m != None)
         mapnik.save_map(self.m, mapFilename)
         label = Labels.Labels(mapFilename)
-        label.writeLabelsXml('[labels]', 'interior', countryFilename, mapFilename)
+        label.writeLabelsXml('[labels]', 'interior',
+                             countryFilename, mapFilename,
+                             maxScale='559082264', minScale='17471321')
 
     def saveImage(self, mapFilename, imgFilename):
         mapnik.load_map(self.m, mapFilename)
@@ -62,6 +69,20 @@ def generateCountryPolygonStyle(filename, opacity, clusterIds):
 
     return s
 
+def generateContourPolygonStyle(opacity, numContours, gamma=1):
+    colorWheel = ["#d2b8e3 ", "#b2cefe", "#baed91", "#faf884", "#f8b88b", "#fd717b", "red"]
+    s = mapnik.Style()
+    for i in range(numContours):
+        r = mapnik.Rule()
+        symbolizer = mapnik.PolygonSymbolizer()
+        symbolizer.fill = mapnik.Color(colorWheel[i])
+        symbolizer.fill_opacity = opacity
+        symbolizer.gamma = gamma
+        r.symbols.append(symbolizer)
+        r.filter = mapnik.Expression('[contourNum].match("' + str(i) + '")')
+        s.rules.append(r)
+    return s
+
 def generateLineStyle(color, opacity):
     s = mapnik.Style()
     r = mapnik.Rule()
@@ -77,4 +98,5 @@ def generateLayer(jsonFile, name, styleName):
     layer = mapnik.Layer(name)
     layer.datasource = ds
     layer.styles.append(styleName)
+    layer.srs = '+init=epsg:4236'
     return layer
