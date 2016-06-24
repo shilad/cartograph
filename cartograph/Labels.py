54b6dd6868c830ddcb358e2d2afb9cf9e02b1010
Monica Ngo <mngo@macalester.edu>
2016-06-24 15:21:30 -0500

    Added shieldSym to labels.

--- cartograph/Labels.py
+++ cartograph/Labels.py
@@ -29,6 +29,24 @@ class Labels():
         textSym.set('face-name', 'DejaVu Sans Book')
         textSym.set('size', '12')
 
+    def _add_Shield_Style(self, field, labelType, minScale, maxScale, imgFile):
+        style = SubElement(self.mapRoot,'Style', name=field[1:-1] + 'LabelStyle')
+        rule = SubElement(style, 'Rule')
+
+        minScaleSym = SubElement(rule, 'MinScaleDenominator')
+        maxScaleSym = SubElement(rule, 'MaxScaleDenominator')
+        minScaleSym.text = str(minScale)
+        maxScaleSym.text = str(maxScale)
+
+        shieldSym = SubElement(rule, 'ShieldSymbolizer', placement=labelType)
+        shieldSym.text = field
+        shieldSym.set('face-name', 'DejaVu Sans Book')
+        shieldSym.set('size', '12')
+        shieldSym.set('dx', '20')
+        shieldSym.set('unlock-image', 'true')
+        shieldSym.set('placement-type', 'simple')
+        shieldSym.set('file', imgFile)
+
     def _add_Text_Layer(self, field, geojsonFile):
         layer = SubElement(self.mapRoot, 'Layer', name=field[1:-1] + 'Layer')
         layer.set('srs', '+init=epsg:4236')
@@ -42,7 +60,12 @@ class Labels():
         dataParamFile = SubElement(data, 'Parameter', name='file')
         dataParamFile.text = geojsonFile
 
-    def writeLabelsXml(self, field, labelType, minScale=1066, maxScale=559082264):
+    def writeLabelsXml(self, field, labelType, minScale='1066', maxScale='559082264'):
         self._add_Text_Style(field, labelType, minScale, maxScale)
+        self._add_Text_Layer(field, self.geojson)
+        self.mapFile.write(self.mapFileName)
+
+    def writeShieldXml(self, field, labelType, imgFile, minScale='1066', maxScale='559082264'):
+        self._add_Shield_Style(field, labelType, minScale, maxScale, imgFile)
         self._add_Text_Layer(field, self.geojson)
         self.mapFile.write(self.mapFileName)
