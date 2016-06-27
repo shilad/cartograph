8fdf7cc53ae375b099349e31c80fc8b812cf83a2
Brooke <bboatman@macalester.edu>
2016-06-27 11:24:19 -0500

    modified build script to take in args, defaults to RenderMap
    Former-commit-id: 293cae0da327fddb9728a1ddd46e653c7818c768

--- build.sh
+++ build.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 export PYTHONPATH=$PYTHONPATH:.
-if luigi --module workflow RenderMap --local-scheduler --retcode-task-failed 1; then
+
+command=""
+
+if [ $# == 0 ]; then
+	command=RenderMap
+elif [ $# == 1 ]; then
+	command="$1"
+fi
+
+if luigi --module workflow $command --local-scheduler --retcode-task-failed 1; then
 	echo "LUIGI BUILD SUCCEEDED" >&2
 	exit 0
 else
