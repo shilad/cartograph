2315d990adc27a4f2a8b0ec3b2b8ee9f6ded96f7
Shilad Sen <Shilad.Sen@target.com>
2016-06-27 16:16:34 -0500

    Remove cache before running tile-stache.
    Former-commit-id: 5329e94fef8f1d9de9b5795164706ea0093a5499

--- run-server.py
+++ run-server.py
@@ -3,13 +3,18 @@
 # in web/ under the http://127.0.0.1:8080/static/ URL.
 #
 
-import os
+import json, os, shutil
 
 from werkzeug.serving import run_simple
 import TileStache
 
 
-import TileStache
-app = TileStache.WSGITileServer(os.path.abspath("./data/tilestache.cfg"))
 static_files =  { '/static': os.path.join(os.path.abspath('./web')) }
+path_cfg = os.path.abspath("./data/tilestache.cfg")
+path_cache = json.load(open(path_cfg, 'r'))['cache']['path']
+assert(len(path_cache) > 5)
+shutil.rmtree(path_cache)
+
+app = TileStache.WSGITileServer(path_cfg)
+
 run_simple('0.0.0.0', 8080, app, static_files=static_files)
