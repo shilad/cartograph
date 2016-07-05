6894ef0761cc430e81f8af7e2c8bfea41717ad52
Shilad Sen <Shilad.Sen@target.com>
2016-07-05 11:58:35 -0500

    Revert "Trying to get gunicorn working."
    This reverts commit d2a9268814d3484c00cb1a2384131e6bd56322a9.

--- cartograph/Server.py
+++ cartograph/Server.py
@@ -1,37 +1,10 @@
-from __future__ import unicode_literals
-
 import json, os, shutil
-import multiprocessing
-import gunicorn.app.base
-from gunicorn.six import iteritems
+
+from werkzeug.serving import run_simple
+from werkzeug.wrappers import Request, Response
 
 import TileStache
 import cartograph
-from ModestMaps.Core import Coordinate
-
-
-
-def number_of_workers():
-    return (multiprocessing.cpu_count() * 2) + 1
-
-
-class StandaloneApplication(gunicorn.app.base.BaseApplication):
-
-    def __init__(self, app, options=None):
-        self.options = options or {}
-        self.application = app
-        super(StandaloneApplication, self).__init__()
-
-    def load_config(self):
-        config = dict([(key, value) for key, value in iteritems(self.options)
-                       if key in self.cfg.settings and value is not None])
-        for key, value in iteritems(config):
-            self.cfg.set(key.lower(), value)
-
-    def load(self):
-        return self.application
-
-
 
 def run_server(path_cfg):
     path_cfg = os.path.abspath(path_cfg)
@@ -43,15 +16,7 @@ def run_server(path_cfg):
         shutil.rmtree(path_cache)
 
     app = CartographServer(path_cfg)
-    print('warming up tile cache...')
-    app.warmup()
-    print('done warming up tile cache...')
-    options = {
-        'bind': '%s:%s' % ('0.0.0.0', '8080'),
-        'static
-        'workers': number_of_workers(),
-    }
-    StandaloneApplication(app, options).run()
+    run_simple('0.0.0.0', 8080, app, static_files=static_files)
 
 
 class CartographServer(TileStache.WSGITileServer):
@@ -62,8 +27,5 @@ class CartographServer(TileStache.WSGITileServer):
             return response(environ, start_response)
         else:
             return TileStache.WSGITileServer.__call__(self, environ, start_response)
-
-    def warmup(self):
-        layer = TileStache.requestLayer(self.config, '/map/0/0/0.png').getTileResponse(Coordinate(0, 0, 0), 'png')
 
 
