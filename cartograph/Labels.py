7b2942c9e88c08bd344b10b2246c5c0dada9fdae
Anja Beth <aswoap@macalester.edu>
2016-07-13 14:52:46 -0500

    yay functional clickinggit add cartograph/Labels.py
    Former-commit-id: 29c2f1519578bb16c5612a9d4f144dab9604d92c

--- cartograph/Labels.py
+++ cartograph/Labels.py
@@ -47,7 +47,7 @@ class Labels():
         for b in range(numBins):
             rule = SubElement(style, 'Rule')
             filterBy = SubElement(rule, 'Filter')
-            filterBy.text = "[maxzoom] = " + str(filterZoomNum) + " and [popbinscore] = " + str(b) + ""
+            filterBy.text = "[maxzoom] <= " + str(filterZoomNum) + " and [popbinscore] = " + str(b) + ""
 
             minScaleSym = SubElement(rule, 'MinScaleDenominator').text = '2133'
             maxScaleSym = SubElement(rule, 'MaxScaleDenominator')
@@ -70,7 +70,7 @@ class Labels():
         for c in range(numBins):
             rule = SubElement(style, 'Rule')
             filterBy = SubElement(rule, 'Filter')
-            filterBy.text = "[maxzoom] = " + str(filterZoomNum) + " and [popbinscore] = " + str(b) + ""
+            filterBy.text = "[maxzoom] <= " + str(filterZoomNum) + " and [popbinscore] = " + str(b) + ""
 
             minScaleSym = SubElement(rule, 'MinScaleDenominator').text = '2133'
             maxScaleSym = SubElement(rule, 'MaxScaleDenominator')
@@ -104,10 +104,8 @@ class Labels():
             layer = SubElement(self.mapRoot, 'Layer', name=field[1:-1] + str(z) + 'Layer')
             layer.set('srs', '+init=epsg:4236')
             layer.set('cache-features', 'true')
-            layer.set('minzoom', self.getMinDenominator(z+1))
-            print self.getMinDenominator(z+1)
+            layer.set('minzoom', self.getMinDenominator(z))
             layer.set('maxzoom', self.getMaxDenominator(z))
-            print self.getMaxDenominator(z) + "\n\n\n"
             addStyle = SubElement(layer, 'StyleName')
             addStyle.text = field[1:-1] + str(z) + 'LabelStyle'
             self.addDataSource(layer, '(select * from ' + self.table + ' where maxzoom <= ' + str(z) + ' order by maxzoom) as foo')
