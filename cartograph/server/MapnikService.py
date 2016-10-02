ac9470a76c1c3ff1eda6a9c07c880c380b03978f
Shilad Sen <Shilad.Sen@target.com>
2016-10-02 01:14:11 -0500

    Added preloader to MapnikService.

--- cartograph/server/MapnikService.py
+++ cartograph/server/MapnikService.py
@@ -1,10 +1,16 @@
+import logging
 import os
 import tempfile
+import threading
 
 import cairo
 import mapnik
 import time
 
+import sys
+
+
+from Queue import Queue
 from cartograph import Config
 
 from math import pi, cos, sin, log, exp, atan
@@ -148,14 +154,80 @@ class MapnikService:
             # cr.stroke()
 
 
+class RenderThread:
+    def __init__(self, conf, q, logLock):
+        self.conf = conf
+        self.logLog = logLock
+        self.q = q
+        self.pointService = PointService(conf)
+        self.mapnik = MapnikService(conf, self.pointService)
+
+
+    def loop(self):
+        while True:
+            # Fetch a tile from the queue and render it
+            r = self.q.get()
+            if (r == None):
+                self.q.task_done()
+                break
+            else:
+                (name, tile_uri, z, x, y) = r
+
+            exists = ""
+            if os.path.isfile(tile_uri):
+                exists = "exists"
+            else:
+                self.mapnik.renderTile(name, z, x, y, tile_uri)
+            bytes = os.stat(tile_uri)[6]
+            empty = ''
+            if bytes == 103:
+                empty = " Empty Tile "
+            self.logLog.acquire()
+            logging.info('creating ' + tile_uri)
+            self.logLog.release()
+            self.q.task_done()
+
+
+
+def render(conf):
+    maxZoom = conf.getint('Server', 'vector_zoom')
+    num_threads = 4
+    # Launch rendering threads
+    queue = Queue(32)
+    logLock = threading.Lock()
+    renderers = {}
+    for i in range(num_threads):
+        renderer = RenderThread(conf, queue, logLock)
+        render_thread = threading.Thread(target=renderer.loop)
+        render_thread.start()
+        # print "Started render thread %s" % render_thread.getName()
+        renderers[i] = render_thread
+
+    cacheDir = conf.get('DEFAULT', 'webCacheDir')
+    for z in range(1, maxZoom + 1):
+        for x in range(2 ** z):
+            for y in range(2 ** z):
+                try:
+                    path = cacheDir + '/raster/%d/%d/%d.png' % (z, x, y)
+                    d = os.path.dirname(path)
+                    if d and not os.path.isdir(d): os.makedirs(d)
+                    t = ('gender', path, z, x, y)
+                    queue.put(t)
+                except KeyboardInterrupt:
+                    raise SystemExit("Ctrl-c detected, exiting...")
+
+    # Signal render threads to exit by sending empty request to queue
+    for i in range(num_threads):
+        queue.put(None)
+    # wait for pending rendering jobs to complete
+    queue.join()
+    for i in range(num_threads):
+        renderers[i].join()
+
 if __name__ == '__main__':
-    conf = Config.initConf('data/conf/simple.txt')
-    ps = PointService(conf)
-    s = MapnikService(conf, ps)
-    t0 = time.time()
-    s.renderTile('gender', 2, 1, 1, 'tile1.png')
-    print time.time() - t0
-    os.system('open tile1.png')
+    logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+    conf = Config.initConf(sys.argv[1])
+    render(conf)
 
 
 
