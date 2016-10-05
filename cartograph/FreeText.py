7ed279b7e3cb56f839417f09477b8a1d6753f376
Shilad Sen <Shilad.Sen@target.com>
2016-10-04 22:59:35 -0500

    Use minified tangram.
    Former-commit-id: 7feee84a074d2fd7fa1630addf18228513af1885

---
+++ cartograph/FreeText.py
@@ -0,0 +1,14 @@
+import luigi
+
+from cartograph.LuigiUtils import MTimeMixin
+
+
+class FreeText:
+    def __init__(self, pathVectors):
+        pass
+
+
+
+class FreeTextTask(MTimeMixin, luigi.Task):
+    def __init__(self):
+        pass
