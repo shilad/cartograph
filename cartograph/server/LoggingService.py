a6556fa7a6230de5800a0e1e15359c614ba359d2
Shilad Sen <Shilad.Sen@target.com>
2016-10-06 22:43:26 -0500

    Lazily initialize logging.

--- cartograph/server/LoggingService.py
+++ cartograph/server/LoggingService.py
@@ -11,21 +11,23 @@ import falcon
 class LoggingService:
     def __init__(self, config):
         self.config = config
-        logDir = config.get('DEFAULT', 'externalDir') + '/logs'
-        if not os.path.isdir(logDir):
+        self.logDir = config.get('DEFAULT', 'externalDir') + '/logs'
+        if not os.path.isdir(self.logDir):
             try:
-                os.makedirs(logDir)
+                os.makedirs(self.logDir)
             except OSError: pass
 
-        now = datetime.datetime.now()
-        name = re.sub('[^0-9]', '_', now.isoformat()) + '_' + str(random.randrange(100000, 999999)) + '.tsv'
-        path = logDir + '/' + name
-        print 'logfile is', path
-        if not os.path.isfile(path):
-            self.log = open(path, 'w')
-            return
+        # These are lazily initialized.
+        self.path = None
+        self.log = None
 
     def on_post(self, req, resp):
+        if not self.path:
+            now = datetime.datetime.now()
+            name = re.sub('[^0-9]', '_', now.isoformat()) + '_' + str(random.randrange(100000, 999999)) + '.tsv'
+            self.path = self.logDir + '/' + name
+            assert not os.path.isfile(self.path)
+            self.log = open(self.path, 'w')
         try:
             js = json.load(req.stream)
             js['ip'] = req.access_route
