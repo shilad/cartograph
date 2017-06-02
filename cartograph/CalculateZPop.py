11c52379573eecd1fc7557e442c2cc9091aabd5a
giangduong36 <gduong@macalester.edu>
2017-06-02 13:48:44 -0500

    Add basic test
    Former-commit-id: 935a0b3898db295887facb95920e9c059b7df989

--- cartograph/CalculateZPop.py
+++ cartograph/CalculateZPop.py
@@ -9,6 +9,7 @@ from geojson import Feature, FeatureCollection
 from geojson import dump, Point
 
 import numpy as np
+import pandas as pd
 
 class CalculateZPopCode(MTimeMixin, luigi.ExternalTask):
     def output(self):
@@ -35,19 +36,29 @@ class ZPopTask(MTimeMixin, luigi.Task):
 
     def run(self):
         config = Config.get()
-        feats = Utils.read_features(config.get("ExternalFiles",
-                                              "popularity"))
-        for f in feats.values():
-            f['popularity'] = float(f.get('popularity', 0.0))
+        # feats = Utils.read_features(config.get("ExternalFiles",
+        #                                       "popularity"))
+        # for f in feats.values():
+        #     f['popularity'] = float(f.get('popularity', 0.0))
+        #
+        # def log4(x): return np.log2(x) / np.log2(4)
+        #
+        # ids = list(feats.keys())
+        # ids.sort(key=lambda i: float(feats[i]['popularity']), reverse=True)
+        # zpops = list(log4(np.arange(len(ids)) / 2.0 + 1.0))
+        #
+        # Utils.write_tsv(config.get("GeneratedFiles", "zpop_with_id"),
+        #                 ("index", "zpop"), ids, zpops)
+
+        feats = pd.read_table(config.get("ExternalFiles", "popularity"), index_col=0)
+        feats = feats.sort_values(['popularity'], ascending=False)
 
         def log4(x): return np.log2(x) / np.log2(4)
 
-        ids = list(feats.keys())
-        ids.sort(key=lambda i: float(feats[i]['popularity']), reverse=True)
-        zpops = list(log4(np.arange(len(ids)) / 2.0 + 1.0))
-
-        Utils.write_tsv(config.get("GeneratedFiles", "zpop_with_id"),
-                        ("index", "zpop"), ids, zpops)
+        zpops = list(log4(np.arange(len(feats)) / 2.0 + 1.0))
+        feats['zpops'] = zpops
+        feats = feats.rename(columns={'ids': 'index'})
+        feats.to_csv(config.get("GeneratedFiles", "test_zpop_with_id"), sep='\t', columns=['zpops'], index_label='index')
 
 
 def test_zpop_task():
@@ -57,6 +68,7 @@ def test_zpop_task():
 
     zpt = ZPopTask()
     zpt.run()
+
 
     assert zpt is not None
 
