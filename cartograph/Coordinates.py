d7b348143498014c6d25be88cc3bb01df1a59e88
giangduong36 <gduong@macalester.edu>
2017-06-09 10:52:23 -0500

    Pandas for Coordinates.
    Former-commit-id: 4489694276529294e0fa308078df0e3428157afa

--- cartograph/Coordinates.py
+++ cartograph/Coordinates.py
@@ -4,11 +4,13 @@ import random
 import luigi
 import numpy as np
 from tsne import bh_sne
+import pandas as pd
+import filecmp
 
 import FastKnn, Utils, Config
 from LuigiUtils import MTimeMixin, TimestampedLocalTarget, getSampleIds
 from PreReqs import WikiBrainNumbering
-from cartograph.PreReqs import SampleCreator
+from PreReqs import SampleCreator
 
 logger = logging.getLogger('cartograph.coordinates')
 
@@ -18,10 +20,11 @@ class CreateEmbedding(MTimeMixin, luigi.Task):
     Use TSNE to reduce high dimensional vectors to x, y coordinates for
     mapping purposes
     '''
+
     def output(self):
         config = Config.get()
         return TimestampedLocalTarget(config.getSample("ExternalFiles",
-                                            "article_embedding"))
+                                                       "article_embedding"))
 
     def requires(self):
         config = Config.get()
@@ -31,19 +34,50 @@ class CreateEmbedding(MTimeMixin, luigi.Task):
         )
 
     def run(self):
+        # config = Config.get()
+        # # Create the embedding.
+        # featureDict = Utils.read_features(config.getSample("ExternalFiles",
+        #                                                    "best_vecs_with_id"),
+        #                                   id_set=getSampleIds())
+        # keys = list(featureDict.keys())
+        # vectors = np.array([featureDict[vID]["vector"] for vID in keys])
+        # out = bh_sne(vectors,
+        #              pca_d=None,
+        #              perplexity=config.getfloat("PreprocessingConstants", "tsne_perplexity"),
+        #              theta=config.getfloat("PreprocessingConstants", "tsne_theta"))
+        # X, Y = list(out[:, 0]), list(out[:, 1])
+        # Utils.write_tsv(config.getSample("ExternalFiles", "article_embedding"),
+        #                 ("index", "x", "y"), keys, X, Y)
+
         config = Config.get()
         # Create the embedding.
-        featureDict = Utils.read_features(config.getSample("ExternalFiles",
-                                                          "best_vecs_with_id"),
-                                          id_set=getSampleIds())
-        keys = list(featureDict.keys())
-        vectors = np.array([featureDict[vID]["vector"] for vID in keys])
+        featureDict = pd.read_table(config.getSample("ExternalFiles",
+                                                     "best_vecs_with_id"), skiprows=1, skip_blank_lines=True, header=None)
+        featureDict['vectorTemp'] = featureDict.iloc[:, 1:].apply(lambda x: tuple(x), axis=1)  # join all vector columns into same column as a tuplevecs['vector'] = vecs['vector'].tolist()
+        featureDict.drop(featureDict.columns[1:-1], axis=1, inplace=True)  # drop all columns but the index and the vectorTemp column
+        featureDict.columns = ['index', 'vector']  # print type(vecs['vector'])
+        featureDict = featureDict.set_index('index')
+        featureDict = featureDict.loc[featureDict.index.isin(getSampleIds())]
+
+        vectors = [list(i) for i in featureDict['vector']]
+        vectors = np.array(vectors)
+
         out = bh_sne(vectors,
                      pca_d=None,
+                     perplexity=config.getfloat("PreprocessingConstants", "tsne_perplexity"),
                      theta=config.getfloat("PreprocessingConstants", "tsne_theta"))
         X, Y = list(out[:, 0]), list(out[:, 1])
+
         Utils.write_tsv(config.getSample("ExternalFiles", "article_embedding"),
-                        ("index", "x", "y"), keys, X, Y)
+                        ("index", "x", "y"), featureDict.index, X, Y)
+
+
+def test_CreateEmbedding_task():
+    Config.initTest()
+    # Create a unit test config object
+    testEmbed = CreateEmbedding()
+    testEmbed.run()
+    assert testEmbed is not None
 
 
 class CreateFullAnnoyIndex(MTimeMixin, luigi.Task):
@@ -86,10 +120,11 @@ class CreateSampleCoordinates(MTimeMixin, luigi.Task):
     Use TSNE to reduce high dimensional vectors to x, y coordinates for
     mapping purposes.
     '''
+
     def output(self):
         config = Config.get()
         return TimestampedLocalTarget(config.getSample("GeneratedFiles",
-                                            "article_coordinates"))
+                                                       "article_coordinates"))
 
     def requires(self):
         return CreateEmbedding()
@@ -98,19 +133,42 @@ class CreateSampleCoordinates(MTimeMixin, luigi.Task):
         config = Config.get()
 
         # Rescale sampled embedded points
-        points = Utils.read_features(config.getSample("ExternalFiles",
-                                               "article_embedding"))
-        keys = list(points.keys())
-        X = [float(points[k]['x']) for k in keys]
-        Y = [float(points[k]['y']) for k in keys]
-        maxVal = max(abs(v) for v in X + Y)
+        points = pd.read_table(config.getSample("ExternalFiles",
+                                                "article_embedding"), index_col='index')
+        points.sort_index(inplace=True)
+        X = pd.np.array(points['x'])
+        Y = pd.np.array(points['y'])
+        maxVal = max(abs(np.append(X, Y)))
+
         scaling = config.getint("MapConstants", "max_coordinate") / maxVal
         X = [x * scaling for x in X]
         Y = [y * scaling for y in Y]
 
         Utils.write_tsv(config.getSample("GeneratedFiles",
-                                  "article_coordinates"),
-                        ("index", "x", "y"), keys, X, Y)
+                                         "article_coordinates"),
+                        ("index", "x", "y"), points.index, X, Y)
+        points['x'] = X
+        points['y'] = Y
+        points.to_csv(config.getSample("GeneratedFiles", "article_coordinates"), sep='\t', index_label='index',
+                      columns=['x', 'y'])
+
+
+def test_CreateSample_task():
+    config = Config.initTest()
+
+    # Create a unit test config object
+    testSample = CreateSampleCoordinates()
+    testSample.run()
+    assert testSample is not None
+
+    result = pd.read_table(config.getSample("GeneratedFiles", "article_coordinates"), index_col='index')
+    correct = pd.read_table('./data/test/tsv/coordinates.sample_50000_correct.tsv', index_col='index')
+
+    tol = 0.01
+    for (id1, row1), (id2, row2) in zip(correct.iterrows(), result.iterrows()):
+        assert id1 == id2
+        assert abs(row1['x'] - row2['x']) < tol
+        assert abs(row1['y'] - row2['y']) < tol
 
 
 class CreateFullCoordinates(MTimeMixin, luigi.Task):
@@ -123,12 +181,23 @@ class CreateFullCoordinates(MTimeMixin, luigi.Task):
 
     def run(self):
         config = Config.get()
-        sampleCoords = Utils.read_features(config.getSample("GeneratedFiles", "article_coordinates"),
-                                           required=('x', 'y'))
-        vecs = Utils.read_features(config.get("ExternalFiles", "vecs_with_id"))
+
+        sampleCoords = pd.read_table(config.getSample("GeneratedFiles",
+                                                      "article_coordinates"), index_col='index')
+        sampleCoords.dropna(axis=0, how='any', inplace=True)
+
+        vecs = pd.read_table(config.get("ExternalFiles", "vecs_with_id"), skip_blank_lines=True, skiprows=1,
+                             header=None)
+        vecs['vectorTemp'] = vecs.iloc[:, 1:].apply(lambda x: tuple(x),
+                                                    axis=1)  # join all vector columns into same column as a tuplevecs['vector'] = vecs['vector'].tolist()
+        vecs.drop(vecs.columns[1:-1], axis=1, inplace=True)  # drop all columns but the index and the vectorTemp column
+        vecs.columns = ['index', 'vector']  # print type(vecs['vector'])
+        vecs = vecs.set_index('index')
+
         knn = FastKnn.FastKnn(config.getSample("ExternalFiles", "vecs_with_id"))
-        assert(knn.exists())
+        assert (knn.exists())
         knn.read()
+
         ids = []
         X = []
         Y = []
@@ -139,21 +208,23 @@ class CreateFullCoordinates(MTimeMixin, luigi.Task):
             return (dx * dx + dy * dy) ** 0.5
 
         threshold = config.getfloat('MapConstants', 'max_coordinate') / 100.0
-        noise = threshold / 10.0    # for points with only one surrogate, add this much random noise
+        noise = threshold / 10.0  # for points with only one surrogate, add this much random noise
 
-        for i, (id, row) in enumerate(vecs.items()):
-            if i % 10000 == 0:
-                logger.info('interpolating coordinates for point %d of %d' % (i, len(vecs)))
-            if id in sampleCoords:
-                x = float(sampleCoords[id]['x'])
-                y = float(sampleCoords[id]['y'])
+        j = 0  # For logging
+        for id, row in vecs.iterrows():
+            if j % 10000 == 0:
+                logger.info('interpolating coordinates for point %d of %d' % (id, len(vecs)))
+            if id in sampleCoords.index:
+                x = float(sampleCoords.ix[id, 'x'])
+                y = float(sampleCoords.ix[id, 'y'])
             else:
                 if len(row['vector']) == 0: continue
                 centroids = []
                 for id2, score in knn.neighbors(row['vector'], 10):
-                    if id2 not in sampleCoords: continue
-                    x = float(sampleCoords[id2]['x'])
-                    y = float(sampleCoords[id2]['y'])
+                    id2 = int(id2)
+                    if id2 not in sampleCoords.index: continue
+                    x = float(sampleCoords.ix[id2, 'x'])
+                    y = float(sampleCoords.ix[id2, 'y'])
                     if score >= 0.0:
                         closestIndex = -1
                         closestDist = 1000000000000
@@ -183,11 +254,37 @@ class CreateFullCoordinates(MTimeMixin, luigi.Task):
                 if n == 1:
                     x += random.uniform(-noise, +noise)
                     y += random.uniform(-noise, +noise)
-
             X.append(x)
             Y.append(y)
             ids.append(id)
+            j += 1
 
-        Utils.write_tsv(config.get("GeneratedFiles",
-                                  "article_coordinates"),
-                        ("index", "x", "y"), ids, X, Y)
+        result = pd.DataFrame()
+        result['x'] = X
+        result['y'] = Y
+        result['index'] = ids
+
+        result.set_index('index', inplace=True)
+        result.to_csv(config.get("GeneratedFiles", "article_coordinates"), sep='\t', index_label='index',
+                      columns=['x', 'y'])
+
+
+def test_createCoordinates_task():
+    config = Config.initTest()
+
+    knn = FastKnn.FastKnn(config.getSample("ExternalFiles", "vecs_with_id"))
+    knn.rebuild()
+
+    # Create a unit test config object
+    testCoor = CreateFullCoordinates()
+    testCoor.run()
+    assert testCoor is not None
+
+    correct = pd.read_table('./data/test/tsv/coordinates_correct.tsv', index_col='index')
+    result = pd.read_table('./data/test/tsv/coordinates.tsv', index_col='index')
+
+    tol = 0.1
+    for (id1, row1), (id2, row2) in zip(correct.iterrows(), result.iterrows()):
+        assert id1 == id2
+        assert abs(row1['x'] - row2['x']) < tol
+        assert abs(row1['y'] - row2['y']) < tol
