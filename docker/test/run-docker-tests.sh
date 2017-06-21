2ec5f390775917e779cbbad389d6b273dc94111c
Shilad Sen <Shilad.Sen@target.com>
2017-06-20 23:40:12 -0500

    Trying to get travis going...
    Former-commit-id: 75a51892673e26f63733751146b41a258fe2000f

--- docker/test/run-docker-tests.sh
+++ docker/test/run-docker-tests.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 # This is run from the root directory, even though it appears here.
+# All tests run from Travis should appear here.
 
 ./build.sh  --conf ./data/conf/integration_test.txt
