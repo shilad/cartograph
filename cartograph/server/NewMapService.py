86c6e3ac238be7ef671147dd17b89a48169b229a
Connor Boyle <connor.bo@gmail.com>
2017-06-26 13:51:54 -0500

    Generate File with Metric Data On Post
    Also now requires that the content of 'articles' in the post request basically be a TSV file (with headers).

--- cartograph/server/NewMapService.py
+++ cartograph/server/NewMapService.py
@@ -1,4 +1,6 @@
 import csv
+import StringIO
+
 import falcon
 import os
 import string
@@ -38,7 +40,6 @@ def filter_tsv(source_dir, target_dir, ids, filename):
     :param target_dir:
     :param ids: an iterable of the ids
     :param filename: the name of the file in <source_dir> to be filtered into a file of the same name in <target_dir>
-    :return:
     """
 
     with codecs.open(os.path.join(source_dir, filename), 'r') as read_file, \
@@ -76,7 +77,7 @@ def gen_config(map_name):
     return config_path
 
 
-def gen_data(map_name, articles):
+def gen_data(map_name, articles_file, metric_type):
     """Generate the data files (i.e. "TSV" files) for a map named <map_name> and a string of articles <articles>.
 
     :param map_name: name of new map
@@ -93,23 +94,41 @@ def gen_data(map_name, articles):
             name = unicode(row[1], encoding='utf-8')
             name_dict[name] = row[0]
 
-    # Generate list of IDs for article names in user request
-    ids = []
-    bad_articles = []
-    for term in articles:
-        try:
-            ids += [name_dict[term]]  # Attempts to find entry in dict of Articles to IDs
-        except KeyError:
-            bad_articles += [term]
-
     # Create the destination directory (if it doesn't exist already)
     target_path = os.path.join(BASE_PATH, 'user/', map_name)
     if not os.path.exists(target_path):
         os.makedirs(target_path)
 
+    # Generate data matrix
+    data = []
+    articles_reader = csv.reader(articles_file, delimiter='\t')
+    header = articles_reader.next()
+    for row in articles_reader:
+        data.append(row)
+
+    # Generate list of IDs for article names in user request
+    ids = []
+    bad_articles = []
+    for row in data:
+        title = row[0]
+        try:
+            ids.append(name_dict[title])  # Attempts to find entry in dict of Articles to IDs
+        except KeyError:
+            bad_articles.append(title)
+
+
     # For each of the data files, filter it and output it to the target directory
     for filename in ['ids.tsv', 'links.tsv', 'names.tsv', 'popularity.tsv', 'vectors.tsv']:
         filter_tsv(SOURCE_DIR, target_path, ids, filename)
+
+    # Generate a data file with metric data
+    with codecs.open(os.path.join(target_path, 'metric.tsv'), 'w') as metric_file:
+        metric_writer = csv.writer(metric_file, delimiter='\t', lineterminator='\n')
+        metric_writer.writerow(header)
+        for row in data:
+            title = row[0]
+            id = name_dict[title]
+            metric_writer.writerow([id]+row[1:])
 
     return bad_articles
 
@@ -128,14 +147,18 @@ class AddMapService:
         resp.body = ''
 
         map_name = post_data['name']
-        articles = post_data['articles'].split('\r\n')
+        metric = post_data['metric']
+        # FIXME: non-ASCII compatible?
+        articles_file = StringIO.StringIO(post_data['articles'])
+
 
         # Prevent adding a map with the same name as a currently-served map
         # This will prevent adding user-generated maps with the same names as
         # active non-user-generated maps, e.g. "simple" or "en"
         assert map_name not in self.map_services.keys()
 
-        bad_articles = gen_data(map_name, articles)
+        # TODO: Figure out what to do with <bad_articles>
+        bad_articles = gen_data(map_name, articles_file, metric)
         config_path = gen_config(map_name)
 
         # Build from the new config file
