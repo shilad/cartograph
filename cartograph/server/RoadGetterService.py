5e65ef6e6eaf7194f6cb14ff6bf79576204dd137
Talha Ahsan <stwyde@gmail.com>
2017-07-05 14:02:38 -0500

    inverted xy

--- cartograph/server/RoadGetterService.py
+++ cartograph/server/RoadGetterService.py
@@ -72,8 +72,8 @@ class RoadGetterService:
         paths = []
         for pathId in pathIds:
             [src, dest] = self.edgeIDPath[pathId]
-            srcCord = self.originalVertices[src] #both of the Cord vals are arrays [x,y]
-            dstCord = self.originalVertices[dest]
+            srcCord = self.originalVertices[src[1], src[0]] #both of the Cord vals are arrays [y,x]
+            dstCord = self.originalVertices[dest[1], dest[0]]
             paths.append([src, "Jek", srcCord])
             paths.append([dest, "Lyndunn", dstCord])
         print(paths)
