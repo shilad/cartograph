15bf802b3f11d775101078bcd4ccc4f620f36d76
cascadianblue <connor.bo@gmail.com>
2017-11-20 10:07:18 -0600

    Soft Matching Sort of Works

--- cartograph/IdFinder.py
+++ cartograph/IdFinder.py
@@ -5,21 +5,34 @@ from difflib import SequenceMatcher
 
 BASE_REDIRECT_URL = 'https://%s.wikipedia.org/w/api.php?action=query&titles=%%s&redirects&format=json' 
 BASE_SEARCH_URL = 'https://%s.wikipedia.org/w/api.php?action=query&list=search&srsearch=%%s&utf8=&format=json'
+PAGE_BY_ID_URL = 'https://www.wikidata.org/w/api.php?action=wbgetentities&props=sitelinks&ids=Q%s&format=json'
+BASE_CATEGORIES_URL = 'https://%s.wikipedia.org/w/api.php?action=query&pageids=%%s&prop=categories&format=json'
+DISAMBIGUATION_CATEGORY_ID = '1982926'
 
 
 class IdFinder:
-    '''An object of this class is responsible for converting lists of article
-    titles to internal IDs.
+    '''An instance of this class is responsible for converting lists of article
+    titles to internal IDs, given a map of titles to internal ids, external ids
+    to internal ids, and a language code.
     '''
-    def __init__(self, names_to_ids, language_code=''):
-        '''name_dataframe: A pandas dataframe of names to internal IDs; please
-                           do not mutate
-           language_code: Code for the language of the Wikipedia to refer to
+    def __init__(self, names_to_ids, external_ids, language_code=''):
+        '''Initialize an IdFinder.
+
+        names_to_ids: A dictionary of names to internal IDs; do not mutate
+        external_ids: A dictionary of external IDs to internal IDs
+        language_code: Code for the language of the Wikipedia to refer to
         '''
         self.names_to_ids = names_to_ids
+        self.external_ids = external_ids
         self.language_code = language_code
         self.redirect_url = BASE_REDIRECT_URL % language_code
         self.search_url = BASE_SEARCH_URL % language_code
+        self.categories_url = BASE_CATEGORIES_URL % language_code
+
+        # Figure out what how to say "disambiguation page" in the language
+        page_title_data = requests.get(PAGE_BY_ID_URL % (DISAMBIGUATION_CATEGORY_ID,)).json()
+        pages_by_language = page_title_data[u'entities'][u'Q'+unicode(DISAMBIGUATION_CATEGORY_ID)][u'sitelinks']
+        self.disambiguation_category_title = pages_by_language[unicode(language_code)+u'wiki'][u'title']
 
     def get_raw_matches(self, titles):
         '''Find and return dictionary of IDs for titles that exist in name
@@ -53,8 +66,8 @@ class IdFinder:
         bad_titles = set()
         for title in titles:
             response = requests.get(self.redirect_url % urllib.quote(title, safe='')).json()
-            if not response['query']['pages'].values()[0].has_key('missing'):
-                title = response['query']['pages'].values()[0]['title']
+            if not response[u'query'][u'pages'].values()[0].has_key(u'missing'):
+                title = response[u'query'][u'pages'].values()[0][u'title']
                 ids[title] = self.names_to_ids[title]
             else:
                 bad_titles.add(title)
@@ -62,23 +75,34 @@ class IdFinder:
         return ids, bad_titles
 
     def get_soft_matches(self, titles):
+        '''Given an iterable of titles, return a dictionary of titles to internal ids
+        '''
         ids = dict()
         bad_titles = set()
+
         for title in titles:
+            # Search for each title
             response = requests.get(self.search_url % urllib.quote(title, safe='')).json()
-            if len(response[u'query'][u'search']):
-                ids[title] = self.names_to_ids[unicode(response[u'query'][u'search'][0][u'title'])]
-            candidate = [0]
-            if SequenceMatcher(candidate[u'title'], title).ratio() >= 0.75:
-                ids[title] = self.names_to_ids[title]
+
+            # Find the first article whose pageid is in external_ids that is
+            # not a disambiguation
+            for article in response[u'query'][u'search']:
+                page_id = int(article[u'pageid'])
+                if page_id in self.external_ids:
+                    categories_query = requests.get(self.categories_url % urllib.quote(str(page_id), safe='')).json()
+                    try:
+                        categories = [category[u'title'] for category in categories_query[u'query'][u'pages'][unicode(page_id)][u'categories']]
+                        if self.disambiguation_category_title not in categories:
+                            ids[title] = self.external_ids[int(page_id)]
+                    except KeyError:
+                        print(categories_query)
             else:
                 bad_titles.add(title)
 
-            return ids, bad_titles
+        return ids, bad_titles
 
     def get_hard_matches(self, titles):
         ids, bad_titles = self.get_raw_matches(titles)
-        print("IDS so far: %s" % str(ids))
         redirect_ids, bad_titles = self.get_redirects(bad_titles)
         ids.update(redirect_ids)
         return ids, bad_titles
@@ -89,9 +113,9 @@ class IdFinder:
 
 
 if __name__ == '__main__':
-    id_finder = IdFinder({'Car': 1, 'Movie': 5, 'Wikipedia': 123}, language_code='simple')
+    id_finder = IdFinder({u'Car': 1, u'Movie': 5, u'Wikipedia': 123}, {214943: 1, 3486: 5, 27263: 123}, language_code='simple')
 
     print(id_finder.get_raw_matches(['Car', 'Wikipedia']))
     print(id_finder.get_raw_matches(['Car', 'Film', 'Wikipedia']))
     print(id_finder.get_hard_matches(['Car', 'Film', 'Wikipedia']))  # Film = Movie
-    print(id_finder.get_soft_matches(['Film', 'Car', 'Encyclopedia', 'Movies']))
+    print(id_finder.get_soft_matches(['Film', 'Car', 'Encyclopedia', 'Movies', 'Coach']))
